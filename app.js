19b395d9b0f55b16bba974ade2461a151ff61592
Ankush263 <ankushbanik263@gmail.com>
2022-04-07 13:33:38 +0530

    Fix adding items in dynamic route

--- app.js
+++ app.js
@@ -67,14 +67,25 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
   const itemName = req.body.newItem
+  const listName = req.body.list
 
   const item = new Item ({
     name: itemName
   })
 
-  item.save()
+  if(listName === "Today") {
+    item.save()
 
-  res.redirect("/")
+    res.redirect("/")
+  } else {
+    List.findOne({name: listName}, (err, foundList) => {
+      foundList.items.push(item)
+      foundList.save()
+      res.redirect('/' + listName)
+    })
+  }
+
+  
 })
 
 app.post('/delete', (req, res) => {
