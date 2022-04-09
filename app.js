f960ec31e298bb3fed635f4377540a6fb916546b
Ankush263 <ankushbanik263@gmail.com>
2022-04-09 13:44:34 +0530

    update app.js

--- app.js
+++ app.js
@@ -142,22 +142,12 @@ app.get('/about', (req, res) => {
   res.render('about')
 })
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
-app.listen(port);
+let port = process.env.PORT || 3000;
+// if (port == null || port == "") {
+//   port = 3000;
+// }
+// app.listen(port);
 
-
-
-
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
-
-
-
-
-// app.listen(port, () => {
-//   console.log("You are listening to the port 3000")
-// })
+app.listen(port, () => {
+  console.log("You are listening to the port 3000")
+})
